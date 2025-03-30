1736baa8af7e5fecaab6a2ea7f3f89b3112cfd0f
Mateusz Polito <matipolit@gmail.com>
2025-03-30 19:04:25 +0200

    first working prototype

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use axum::{
 use axum_extra::extract::{CookieJar, cookie::Cookie};
 use dotenvy::dotenv;
 use msp::{Conf, MspErr, Server};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use sha2::{
     Digest, Sha256,
     digest::{
@@ -23,9 +23,10 @@ use sha2::{
 };
 use std::env;
 use std::future::Future;
+use std::process::Command;
 
 use tera::Tera;
-use tokio::{sync::broadcast, time::Instant};
+use tokio::sync::broadcast;
 use tracing::Level;
 use tracing_subscriber;
 
@@ -36,7 +37,7 @@ type DigestedHash =
 
 #[derive(Clone)]
 struct AppState {
-    mc_server_broadcast: broadcast::Sender<Option<Server>>,
+    mc_server_broadcast: broadcast::Sender<RealTimeData>,
     server_conf: Conf,
     templates: Tera,
     running_on_subpath: bool,
@@ -53,16 +54,70 @@ struct LoginForm {
     password: String,
 }
 
+#[derive(Serialize, Clone)]
+struct ServerProtocolInfo {
+    version: String,
+    max_players: i32,
+    online_players: i32,
+}
+
+#[derive(Serialize, Clone)]
+struct RealTimeData {
+    protocol_info: Option<ServerProtocolInfo>,
+    active: bool,
+}
+
+fn get_service_active() -> bool {
+    let is_service_active_output = Command::new("systemctl")
+        .arg("is-active")
+        .arg("--user")
+        .arg("minecraft")
+        .output()
+        .expect("Could not get minecraft systemd service status")
+        .stdout;
+    let str_output =
+        String::from_utf8(is_service_active_output).expect("Malformed output from systemctl");
+    let trimmed_output = str_output.trim();
+    tracing::trace!("output from systemctl: {}", trimmed_output);
+    let result = trimmed_output.eq("active");
+    tracing::trace!("returning result: {}", result);
+    return result;
+}
+
+fn toggle_server_service(toggle_variant: String) -> bool {
+    let toggle_service_output = Command::new("systemctl")
+        .arg(toggle_variant)
+        .arg("--user")
+        .arg("minecraft")
+        .output()
+        .expect("Could not toggle minecraft systemd service")
+        .stdout;
+    let str_output =
+        String::from_utf8(toggle_service_output).expect("Malformed output from systemctl");
+    let trimmed_output = str_output.trim();
+    tracing::debug!("output from systemctl: {}", trimmed_output);
+    let result = trimmed_output.eq("");
+    return result;
+}
+
+fn redirect_to(running_on_subpath: bool, destination: String) -> Redirect {
+    if running_on_subpath {
+        Redirect::to(&format!("mc-dash/{}", destination))
+    } else {
+        Redirect::to(&format!("{}", destination))
+    }
+}
+
 #[tokio::main]
 async fn main() {
     if cfg!(debug_assertions) {
         tracing_subscriber::fmt()
-            .with_max_level(Level::TRACE)
+            .with_max_level(Level::DEBUG)
             .init();
     }
     dotenv().expect(".env file not found");
 
-    let server_url = env::var("SERVER_URL").unwrap_or("localhost".to_string());
+    let server_url = "localhost".to_owned();
     let password = env::var("PASSWORD").expect("PASSWORD not set");
     let run_on_subpath_env = env::var("RUN_ON_SUBPATH");
 
@@ -73,7 +128,7 @@ async fn main() {
 
     let server_conf = Conf::create_with_port(&server_url, 25565);
 
-    let (server_broadcast, _) = broadcast::channel::<Option<Server>>(1);
+    let (server_broadcast, _) = broadcast::channel::<RealTimeData>(1);
 
     let templates = Tera::new("templates/**/*").expect("Error initializing Tera");
 
@@ -87,11 +142,24 @@ async fn main() {
 
     tokio::task::spawn_blocking(move || {
         loop {
-            let info_result: Result<Server, MspErr> = server_conf.get_server_status();
-            if let Ok(info) = info_result {
-                tracing::trace!("Broadcasting server info");
-                let _ = server_broadcast.send(Some(info));
-            }
+            let active = get_service_active();
+            let server_result = server_conf.get_server_status();
+            tracing::trace!("Broadcasting server info");
+            let protocol_info = if server_result.is_ok() {
+                let info = server_result.unwrap();
+                Some(ServerProtocolInfo {
+                    max_players: info.players.max,
+                    online_players: info.players.online,
+                    version: info.version.name,
+                })
+            } else {
+                None
+            };
+
+            let _ = server_broadcast.send(RealTimeData {
+                protocol_info,
+                active,
+            });
             std::thread::sleep(Duration::from_secs(5));
         }
     });
@@ -106,6 +174,14 @@ async fn main() {
             )
             .route("/login", get(login_page_get))
             .route("/login", post(login_post))
+            .route("/logout", get(logout_page_get))
+            .route("/realtime-stats", get(realtime_serverinfo_get))
+            .route(
+                "/toggle",
+                get(|state, cookies, query| async {
+                    auth_wrapper(state, cookies, query, toggle_server).await
+                }),
+            )
             .with_state(app_state)
     } else {
         let router = Router::new()
@@ -117,11 +193,18 @@ async fn main() {
             )
             .route("/login", get(login_page_get))
             .route("/login", post(login_post))
+            .route("/logout", get(logout_page_get))
+            .route("/realtime-stats", get(realtime_serverinfo_get))
+            .route(
+                "/toggle",
+                get(|state, cookies, query| async {
+                    auth_wrapper(state, cookies, query, toggle_server).await
+                }),
+            )
             .with_state(app_state);
         Router::new().nest("/mc-dash", router)
     };
 
-    // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
@@ -163,7 +246,7 @@ fn extract_provided(query: &PasswordQuery, cookies: &CookieJar) -> Option<String
 }
 
 async fn auth_wrapper<F, Fut, R>(
-    State(app_state): State<AppState>,
+    State(state): State<AppState>,
     cookies: CookieJar,
     query: Query<PasswordQuery>,
     handler: F,
@@ -174,21 +257,23 @@ where
     R: IntoResponse,
 {
     if let Some(pass) = extract_provided(&query.0, &cookies) {
-        if authenticate(&app_state.auth_hash, &pass) {
-            let response = handler(State(app_state)).await;
+        if authenticate(&state.auth_hash, &pass) {
+            let response = handler(State(state)).await;
+            tracing::debug!("user authenticated, returing desired response");
             return response.into_response();
         }
     }
-    Redirect::to("/login").into_response()
+    tracing::debug!("user not authenticated, redirecting to login");
+
+    redirect_to(state.running_on_subpath, "login".to_owned()).into_response()
 }
 
-async fn main_page_get(State(app_state): State<AppState>) -> Html<String> {
-    let info_result: Result<Server, MspErr> = app_state.server_conf.get_server_status();
-    if let Ok(info) = info_result {
-        Html(format!("<h1>version{:?}</h1>", info.version))
-    } else {
-        Html("<h1>Could not get server info</h1>".into())
-    }
+async fn main_page_get(State(state): State<AppState>) -> Html<String> {
+    let active = get_service_active();
+    let mut context = tera::Context::new();
+    context.insert("active", &active);
+    context.insert("running_on_subpath", &state.running_on_subpath);
+    Html(state.templates.render("index.html", &context).unwrap())
 }
 
 async fn login_page_get(State(app_state): State<AppState>) -> Html<String> {
@@ -200,17 +285,32 @@ async fn login_page_get(State(app_state): State<AppState>) -> Html<String> {
     )
 }
 
+async fn logout_page_get(State(state): State<AppState>, cookies: CookieJar) -> impl IntoResponse {
+    let cookies = cookies.remove(Cookie::from("auth"));
+    let redirect = redirect_to(state.running_on_subpath, "/login".to_owned());
+    (cookies, redirect)
+}
+
+async fn toggle_server(State(state): State<AppState>) -> impl IntoResponse {
+    let active = get_service_active();
+    let toggle_res = if active {
+        toggle_server_service("stop".into());
+    } else {
+        toggle_server_service("start".into());
+    };
+
+    redirect_to(state.running_on_subpath, "/".to_owned())
+}
+
 async fn login_post(
     cookies: CookieJar,
     State(state): State<AppState>,
     Form(form): Form<LoginForm>,
 ) -> impl IntoResponse {
-    let redirect = if state.running_on_subpath {
-        Redirect::to("/mc-dash")
-    } else {
-        Redirect::to("/")
-    };
+    tracing::debug!("login post form received");
+    let redirect = redirect_to(state.running_on_subpath, "/".to_owned());
     if authenticate(&state.auth_hash, &form.password) {
+        tracing::info!("Authentication successful!");
         let cookie = Cookie::build(("auth", form.password))
             .path("/")
             // For web UI usage you may want JS to read it, so not HTTP-only.
@@ -219,6 +319,7 @@ async fn login_post(
         let cookies = cookies.add(cookie);
         (cookies, redirect)
     } else {
+        tracing::info!("Authentication unsuccessful!");
         // On failed login, simply redirect back.
         (cookies, redirect)
     }
